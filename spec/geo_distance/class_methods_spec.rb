e03c41c54086f54886027d153ac22bed399483fb
Kristian Mandrup <kmandrup@gmail.com>
2011-06-22 14:35:41 +0200

    better specs

--- spec/geo_distance/class_methods_spec.rb
+++ spec/geo_distance/class_methods_spec.rb
@@ -1,22 +1,60 @@
 require 'spec_helper'
 
-describe "GeoDistance class methods" do
-  before do
-    from = [-104.88544, 39.06546].geo_point
-    to =   [-104.80, lat1].geo_point
-  end
+GeoPoint.coord_mode = :lng_lat
 
+describe "GeoDistance class methods" do
+  describe '#earth_radius' do
+    it 'should return in kms' do
+      GeoDistance.earth_radius(:kms).should be_between(6371, 6380)
+    end
+
+    it 'should return in meters' do
+      GeoDistance.earth_radius(:meters).should be_between(6371000, 6380000)
+    end
+  end
+  
   describe '#default_algorithm' do
-    it "should work" do
+    let(:from)  { [-104.88544, 39.06546].geo_point }
+    let(:to)    { [-104.80, 39.06546].geo_point }
+
+    it "should NOT set it to :haver" do
+      lambda { GeoDistance.default_algorithm = :haver}.should raise_error
+    end
+
+    it "should set it to :haversine" do
       GeoDistance.default_algorithm = :haversine
 
-      dist = GeoDistance.distance( lat1, lon1, lat2, lon2 )
+      dist = GeoDistance.geo_distance(from, to)
 
-      puts "the distance from  #{lat1}, #{lon1} to #{lat2}, #{lon2} is: #{dist.meters} meters"
+      puts "the distance from #{from} to #{to} is: #{dist.meters} meters"
 
       dist.feet.should == 24193.0
       dist.to_feet.should == 24193.0
       dist.kms.to_s.should match(/7\.376*/)
+    end
+
+    it "should set it to :flat" do
+      GeoDistance.default_algorithm = :flat
+
+      dist = GeoDistance.geo_distance(from, to)
+      puts "the distance from #{from} to #{to} is: #{dist.meters} meters"
+      dist.feet.should be_between(23500, 25000)
+    end
+
+    it "should set it to :sphere" do
+      GeoDistance.default_algorithm = :sphere
+
+      dist = GeoDistance.geo_distance(from, to)
+      puts "the distance from #{from} to #{to} is: #{dist.meters} meters"
+      dist.feet.should be_between(23500, 25000)
+    end
+
+    it "should set it to :vincenty" do
+      GeoDistance.default_algorithm = :vincenty
+
+      dist = GeoDistance.geo_distance(from, to)
+      puts "the distance from #{from} to #{to} is: #{dist.meters} meters"
+      dist.feet.should be_between(23500, 25000)
     end
   end
 end
